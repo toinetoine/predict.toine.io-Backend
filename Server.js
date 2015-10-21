e99a9424338251517e6a8ff5b5189ef3b3df61e0
Your Name <adahan93@gmail.com>
2015-10-20 23:45:43 -0400

    Now checking price every 5 mins and using bulk insert (100 stocks at a time). Currently debugging the prediction check function

--- Server.js
+++ Server.js
@@ -15,18 +15,19 @@ mongoClient.connect(mongoUrl, function (error, database) {
     db = database;
     mongoError = error;
     console.log("starting....");
+    checkActivePredictions();
 });
 
 // configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 
 /**
-  * Every 1 minute, record the price of all of the stocks in the objects table 
-  * (stocks have the type: "stock"). (every 0th second)
+  * Every 5 minutes, record the price of all of the stocks in the objects table 
+  * (stocks have the type: "stock").
   */
-schedule.scheduleJob('0 */2 * * * *', function() {
+schedule.scheduleJob('0 */5 * * * *', function() {
     var symbols = ["MMM", "ABT", "ABBV", "ACN", "ACE", "ATVI", "ADBE", "ADT", "AAP", "AES", "AET", "AFL", "AMG", "A", "GAS", "APD", "ARG", "AKAM", "AA", "AGN", "ALXN", "ALLE", "ADS", "ALL", "GOOGL", "GOOG", "ALTR", "MO", "AMZN", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AMP", "ABC", "AME", "AMGN", "APH", "APC", "ADI", "AON", "APA", "AIV", "AAPL", "AMAT", "ADM", "AIZ", "T", "ADSK", "ADP", "AN", "AZO", "AVGO", "AVB", "AVY", "BHI", "BLL", "BAC", "BK", "BCR", "BXLT", "BAX", "BBT", "BDX", "BBBY", "BRK-B", "BBY", "BIIB", "BLK", "HRB", "BA", "BWA", "BXP", "BSX", "BMY", "BRCM", "BF-B", "CHRW", "CA", "CVC", "COG", "CAM", "CPB", "COF", "CAH", "HSIC", "KMX", "CCL", "CAT", "CBG", "CBS", "CELG", "CNP", "CTL", "CERN", "CF", "SCHW", "CHK", "CVX", "CMG", "CB", "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CTXS", "CLX", "CME", "CMS", "COH", "KO", "CCE", "CTSH", "CL", "CPGX", "CMCSA", "CMCSK", "CMA", "CSC", "CAG", "COP", "CNX", "ED", "STZ", "GLW", "COST", "CCI", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DLPH", "DAL", "XRAY", "DVN", "DO", "DFS", "DISCA", "DISCK", "DG", "DLTR", "D", "DOV", "DOW", "DPS", "DTE", "DD", "DUK", "DNB", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMC", "EMR", "ENDP", "ESV", "ETR", "EOG", "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ES", "EXC", "EXPE", "EXPD", "ESRX", "XOM", "FFIV", "FB", "FAST", "FDX", "FIS", "FITB", "FSLR", "FE", "FISV", "FLIR", "FLS", "FLR", "FMC", "FTI", "F", "FOSL", "BEN", "FCX", "FTR", "GME", "GPS", "GRMN", "GD", "GE", "GGP", "GIS", "GM", "GPC", "GNW", "GILD", "GS", "GT", "GWW", "HAL", "HBI", "HOG", "HAR", "HRS", "HIG", "HAS", "HCA", "HCP", "HP", "HES", "HPQ", "HD", "HON", "HRL", "HST", "HCBK", "HUM", "HBAN", "ITW", "IR", "INTC", "ICE", "IBM", "IP", "IPG", "IFF", "INTU", "ISRG", "IVZ", "IRM", "JEC", "JBHT", "JNJ", "JCI", "JPM", "JNPR", "KSU", "K", "KEY", "GMCR", "KMB", "KIM", "KMI", "KLAC", "KSS", "KHC", "KR", "LB", "LLL", "LH", "LRCX", "LM", "LEG", "LEN", "LVLT", "LUK", "LLY", "LNC", "LLTC", "LMT", "L", "LOW", "LYB", "MTB", "MAC", "M", "MNK", "MRO", "MPC", "MAR", "MMC", "MLM", "MAS", "MA", "MAT", "MKC", "MCD", "MHFI", "MCK", "MJN", "WRK", "MDT", "MRK", "MET", "KORS", "MCHP", "MU", "MSFT", "MHK", "TAP", "MDLZ", "MON", "MNST", "MCO", "MS", "MOS", "MSI", "MUR", "MYL", "NDAQ", "NOV", "NAVI", "NTAP", "NFLX", "NWL", "NFX", "NEM", "NWSA", "NWS", "NEE", "NLSN", "NKE", "NI", "NBL", "JWN", "NSC", "NTRS", "NOC", "NRG", "NUE", "NVDA", "ORLY", "OXY", "OMC", "OKE", "ORCL", "OI", "PCAR", "PH", "PDCO", "PAYX", "PYPL", "PNR", "PBCT", "POM", "PEP", "PKI", "PRGO", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PBI", "PCL", "PNC", "RL", "PPG", "PPL", "PX", "PCP", "PCLN", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PSA", "PHM", "PVH", "QRVO", "PWR", "QCOM", "DGX", "RRC", "RTN", "O", "RHT", "REGN", "RF", "RSG", "RAI", "RHI", "ROK", "COL", "ROP", "ROST", "RCL", "R", "CRM", "SNDK", "SCG", "SLB", "SNI", "STX", "SEE", "SRE", "SHW", "SIAL", "SIG", "SPG", "SWKS", "SLG", "SJM", "SNA", "SO", "LUV", "SWN", "SE", "STJ", "SWK", "SPLS", "SBUX", "HOT", "STT", "SRCL", "SYK", "STI", "SYMC", "SYY", "TROW", "TGT", "TEL", "TE", "TGNA", "THC", "TDC", "TSO", "TXN", "TXT", "HSY", "TRV", "TMO", "TIF", "TWX", "TWC", "TJX", "TMK", "TSS", "TSCO", "RIG", "TRIP", "FOXA", "FOX", "TSN", "TYC", "USB", "UA", "UNP", "UAL", "UNH", "UPS", "URI", "UTX", "UHS", "UNM", "URBN", "VFC", "VLO", "VAR", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "VIAB", "V", "VNO", "VMC", "WMT", "WBA", "DIS", "WM", "WAT", "ANTM", "WFC", "HCN", "WDC", "WU", "WY", "WHR", "WFM", "WMB", "WEC", "WYN", "WYNN", "XEL", "XRX", "XLNX", "XL", "XYL", "YHOO", "YUM", "ZBH", "ZION", "ZTS"];
-    for(var queryIndex = 0; queryIndex <= Math.floor((symbols.length-1)/20); queryIndex++) {
+    for(var queryIndex = 0; queryIndex <= Math.floor((symbols.length-1)/100); queryIndex++) {
         // delay the query in order to create even distribution of queries over the entire minute period
         // (send a query every 2 seconds)
         setTimeout(function (symbolsToGrab, isLastSymbol) {
@@ -38,8 +39,8 @@ schedule.scheduleJob('0 */2 * * * *', function() {
                     checkActivePredictions();
                 }
             }
-        }(symbols.slice(queryIndex * 20, (queryIndex + 1) * 20), queryIndex == Math.floor((symbols.length - 1) / 20)),
-        queryIndex * 3000);
+        }(symbols.slice(queryIndex * 100, (queryIndex + 1) * 100), queryIndex == Math.floor((symbols.length - 1) / 100)),
+        queryIndex * 10000); // stagger find-and-store prices opertation each by 10 seconds
     }
 });
 
@@ -71,19 +72,20 @@ var grabAndInsertNewStockValues = function (symbols) {
 
     queryYQL.exec(function (err, data) {
         if (!mongoError && data != null && data.hasOwnProperty("query") && data.query.hasOwnProperty("results")) {
+            var currentTime = Math.floor((new Date).getTime() / 60000) * 60;
+
+            var newPriceDocuments = Array();
             for (var resultIndex = 0; resultIndex < data.query.results.quote.length; resultIndex++) {
                 var thisQuote = data.query.results.quote[resultIndex];
-                var currentTime = Math.floor((new Date).getTime() / 60000) * 60;
-                console.log("Grabbing: " + thisQuote);
-                // insert the stock price into the values collection
-                var valuesCollection = db.collection('values');
-                valuesCollection.insertOne({
-                    object: thisQuote.Symbol,
-                    type: "stock",
-                    value: thisQuote.LastTradePriceOnly,
-                    time: currentTime
-                }, function (err, result) { });
+                var priceDocument = {};
+                priceDocument.object = thisQuote.Symbol;
+                priceDocument.type = "stock";
+                priceDocument.value = thisQuote.LastTradePriceOnly;
+                priceDocument.time = currentTime;
+                newPriceDocuments.push(priceDocument);
             }
+            var valuesCollection = db.collection('values');
+            valuesCollection.insert(newPriceDocuments, function (err, result) { });
         }
     });
 }
@@ -183,13 +185,13 @@ var checkActivePredictions = function () {
                     predictionUpdateObject.$set.checked = currentTime;
 
 
-                    
+                    console.log(predictionCheckResult.status);
                     if (predictionCheckResult.status != "active") {
                         db.collection('predictions').updateOne(
                           { _id: activePrediction._id },
                           predictionUpdateObject, 
                           function (err, results) {
-                              console.log(results);
+                              //console.log(results);
                           });
                     }
                 });
