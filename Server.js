69b72cb2d01402579e9aafcabda90c230033be0b
Your Name <adahan93@gmail.com>
2015-10-25 01:13:16 -0400

    Time field is now also being added to the historical value being input in the historicalValues collection

--- Server.js
+++ Server.js
@@ -9,6 +9,7 @@ var mongoUrl = 'mongodb://localhost:27017/predict';
 
 var schedule = require('node-schedule');
 
+// symbols in the S&P500
 var symbols = ["MMM", "ABT", "ABBV", "ACN", "ACE", "ATVI", "ADBE", "ADT", "AAP", "AES", "AET", "AFL", "AMG", "A", "GAS", "APD", "ARG", "AKAM", "AA", "AGN", "ALXN", "ALLE", "ADS", "ALL", "GOOGL", "GOOG", "ALTR", "MO", "AMZN", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AMP", "ABC", "AME", "AMGN", "APH", "APC", "ADI", "AON", "APA", "AIV", "AAPL", "AMAT", "ADM", "AIZ", "T", "ADSK", "ADP", "AN", "AZO", "AVGO", "AVB", "AVY", "BHI", "BLL", "BAC", "BK", "BCR", "BXLT", "BAX", "BBT", "BDX", "BBBY", "BRK-B", "BBY", "BIIB", "BLK", "HRB", "BA", "BWA", "BXP", "BSX", "BMY", "BRCM", "BF-B", "CHRW", "CA", "CVC", "COG", "CAM", "CPB", "COF", "CAH", "HSIC", "KMX", "CCL", "CAT", "CBG", "CBS", "CELG", "CNP", "CTL", "CERN", "CF", "SCHW", "CHK", "CVX", "CMG", "CB", "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CTXS", "CLX", "CME", "CMS", "COH", "KO", "CCE", "CTSH", "CL", "CPGX", "CMCSA", "CMCSK", "CMA", "CSC", "CAG", "COP", "CNX", "ED", "STZ", "GLW", "COST", "CCI", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DLPH", "DAL", "XRAY", "DVN", "DO", "DFS", "DISCA", "DISCK", "DG", "DLTR", "D", "DOV", "DOW", "DPS", "DTE", "DD", "DUK", "DNB", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMC", "EMR", "ENDP", "ESV", "ETR", "EOG", "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ES", "EXC", "EXPE", "EXPD", "ESRX", "XOM", "FFIV", "FB", "FAST", "FDX", "FIS", "FITB", "FSLR", "FE", "FISV", "FLIR", "FLS", "FLR", "FMC", "FTI", "F", "FOSL", "BEN", "FCX", "FTR", "GME", "GPS", "GRMN", "GD", "GE", "GGP", "GIS", "GM", "GPC", "GNW", "GILD", "GS", "GT", "GWW", "HAL", "HBI", "HOG", "HAR", "HRS", "HIG", "HAS", "HCA", "HCP", "HP", "HES", "HPQ", "HD", "HON", "HRL", "HST", "HCBK", "HUM", "HBAN", "ITW", "IR", "INTC", "ICE", "IBM", "IP", "IPG", "IFF", "INTU", "ISRG", "IVZ", "IRM", "JEC", "JBHT", "JNJ", "JCI", "JPM", "JNPR", "KSU", "K", "KEY", "GMCR", "KMB", "KIM", "KMI", "KLAC", "KSS", "KHC", "KR", "LB", "LLL", "LH", "LRCX", "LM", "LEG", "LEN", "LVLT", "LUK", "LLY", "LNC", "LLTC", "LMT", "L", "LOW", "LYB", "MTB", "MAC", "M", "MNK", "MRO", "MPC", "MAR", "MMC", "MLM", "MAS", "MA", "MAT", "MKC", "MCD", "MHFI", "MCK", "MJN", "WRK", "MDT", "MRK", "MET", "KORS", "MCHP", "MU", "MSFT", "MHK", "TAP", "MDLZ", "MON", "MNST", "MCO", "MS", "MOS", "MSI", "MUR", "MYL", "NDAQ", "NOV", "NAVI", "NTAP", "NFLX", "NWL", "NFX", "NEM", "NWSA", "NWS", "NEE", "NLSN", "NKE", "NI", "NBL", "JWN", "NSC", "NTRS", "NOC", "NRG", "NUE", "NVDA", "ORLY", "OXY", "OMC", "OKE", "ORCL", "OI", "PCAR", "PH", "PDCO", "PAYX", "PYPL", "PNR", "PBCT", "POM", "PEP", "PKI", "PRGO", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PBI", "PCL", "PNC", "RL", "PPG", "PPL", "PX", "PCP", "PCLN", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PSA", "PHM", "PVH", "QRVO", "PWR", "QCOM", "DGX", "RRC", "RTN", "O", "RHT", "REGN", "RF", "RSG", "RAI", "RHI", "ROK", "COL", "ROP", "ROST", "RCL", "R", "CRM", "SNDK", "SCG", "SLB", "SNI", "STX", "SEE", "SRE", "SHW", "SIAL", "SIG", "SPG", "SWKS", "SLG", "SJM", "SNA", "SO", "LUV", "SWN", "SE", "STJ", "SWK", "SPLS", "SBUX", "HOT", "STT", "SRCL", "SYK", "STI", "SYMC", "SYY", "TROW", "TGT", "TEL", "TE", "TGNA", "THC", "TDC", "TSO", "TXN", "TXT", "HSY", "TRV", "TMO", "TIF", "TWX", "TWC", "TJX", "TMK", "TSS", "TSCO", "RIG", "TRIP", "FOXA", "FOX", "TSN", "TYC", "USB", "UA", "UNP", "UAL", "UNH", "UPS", "URI", "UTX", "UHS", "UNM", "URBN", "VFC", "VLO", "VAR", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "VIAB", "V", "VNO", "VMC", "WMT", "WBA", "DIS", "WM", "WAT", "ANTM", "WFC", "HCN", "WDC", "WU", "WY", "WHR", "WFM", "WMB", "WEC", "WYN", "WYNN", "XEL", "XRX", "XLNX", "XL", "XYL", "YHOO", "YUM", "ZBH", "ZION", "ZTS"];
 
 var db;
@@ -24,8 +25,6 @@ mongoClient.connect(mongoUrl, function (error, database) {
     //getHistoricalData(["AAPL", "GOOG"]);
 });
 
-// symbols in the Fortune 500
-
 // configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -33,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
   * Every 5 minutes, record the price of all of the stocks in the objects table 
   * (stocks have the type: "stock").
   */
-schedule.scheduleJob('0 */5 * * * *', function() {/*
+schedule.scheduleJob('0 */5 * * * *', function() {
     for(var queryIndex = 0; queryIndex <= Math.floor((symbols.length-1)/100); queryIndex++) {
         // delay the query in order to create even distribution of queries over the entire minute period
         // (send a query every 2 seconds)
@@ -48,38 +47,39 @@ schedule.scheduleJob('0 */5 * * * *', function() {/*
             }
         }(symbols.slice(queryIndex * 100, (queryIndex + 1) * 100), queryIndex == Math.floor((symbols.length - 1) / 100)),
         queryIndex * 10000); // stagger find-and-store prices opertation each by 10 seconds
-    }*/
+    }
 });
 
 /**
-  * Grab the historical data for the day before every day at 12:03am
+  * Grab the historical data for the day before every day at 12:13am
   * Every day at 12:13am record the day's high/low for each of the stocks in the historicalValues collection.
   */
 schedule.scheduleJob('0 13 0 * * *', function () {
+    // get the day, month, and year of yesterday
+    var yesterdayDate = new Date();
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1); // get yesterday's date
+    var yesterdayYear = yesterdayDate.getFullYear();
+    var yesterdayMonth = yesterdayDate.getMonth() + 1;
+    var yesterdayDay = yesterdayDate.getDate();
+
     // populate the historical data table in chunks of 100 stocks (every 10 seconds)
     for(var queryIndex = 0; queryIndex <= Math.floor((symbols.length-1)/100); queryIndex++) {
         // delay the query in order to create even distribution of queries over the entire minute period
         // (send a query every 2 seconds)
-        setTimeout(function (symbolsToGrabHistoricalValuesFor) {
+        setTimeout(function (symbolsToGrabHistoricalValuesFor, day, month, year) {
             return function () {
                 console.log("here");
-                getHistoricalData(symbolsToGrabHistoricalValuesFor);
+                getHistoricalData(symbolsToGrabHistoricalValuesFor, day, month, year);
             }
-        }(symbols.slice(queryIndex * 100, (queryIndex + 1) * 100)),
+        }(symbols.slice(queryIndex * 100, (queryIndex + 1) * 100), yesterdayDay, yesterdayMonth, yesterdayYear),
         queryIndex * 10000); // stagger find-and-store prices opertation each by 10 seconds
     }
 });
 
-var getHistoricalData = function(symbolsToGet) {
-    var currentDate = new Date();
-    var yesterdayDate = new Date();
-    yesterdayDate.setDate(yesterdayDate.getDate() - 1); // get yesterday's date
-    var yesterdayYear = yesterdayDate.getFullYear();
-    var yesterdayMonthNumber = yesterdayDate.getMonth() + 1;
-    var yesterdayDay = yesterdayDate.getDate();
+var getHistoricalData = function(symbolsToGet, day, month, year) {
     var queryString = "select * from yahoo.finance.historicaldata where symbol in ('" + symbolsToGet.join("','") + "') and startDate = '" +
-        yesterdayYear.toString() + "-" + yesterdayMonthNumber.toString() + "-" + yesterdayDay.toString() + "' and endDate = '" +
-        yesterdayYear.toString() + "-" + yesterdayMonthNumber.toString() + "-" + yesterdayDay.toString() + "'";
+        year.toString() + "-" + month.toString() + "-" + day.toString() + "' and endDate = '" +
+        year.toString() + "-" + month.toString() + "-" + day.toString() + "'";
     var queryYQL = new YQL(queryString);
     
     // execute the query to retreive the historical values from the day
@@ -96,6 +96,8 @@ var getHistoricalData = function(symbolsToGet) {
                 valueDocument.year = parseInt(thisQuote.Date.split("-")[0]);
                 valueDocument.month = parseInt(thisQuote.Date.split("-")[1]);
                 valueDocument.day = parseInt(thisQuote.Date.split("-")[2]);
+                valueDocument.time = 
+                    (new Date(valueDocument.year, valueDocument.month, valueDocument.day)).getTime()/1000;
                 // set the value doc's values
                 valueDocument.open = parseFloat(thisQuote.Open).toFixed(2);
                 valueDocument.close = parseFloat(thisQuote.Close).toFixed(2);
