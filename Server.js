e91aa1ef08dee935e2f90a46a93400e870c2af55
Your Name <adahan93@gmail.com>
2015-11-16 21:32:45 -0500

    email working

--- Server.js
+++ Server.js
@@ -9,6 +9,10 @@ var mongoUrl = 'mongodb://localhost:27017/predict';
 
 var schedule = require('node-schedule');
 
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+
 // symbols in the S&P500
 var symbols = ["MMM", "ABT", "ABBV", "ACN", "ACE", "ATVI", "ADBE", "ADT", "AAP", "AES", "AET", "AFL", "AMG", "A", "GAS", "APD", "ARG", "AKAM", "AA", "AGN", "ALXN", "ALLE", "ADS", "ALL", "GOOGL", "GOOG", "ALTR", "MO", "AMZN", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AMP", "ABC", "AME", "AMGN", "APH", "APC", "ADI", "AON", "APA", "AIV", "AAPL", "AMAT", "ADM", "AIZ", "T", "ADSK", "ADP", "AN", "AZO", "AVGO", "AVB", "AVY", "BHI", "BLL", "BAC", "BK", "BCR", "BXLT", "BAX", "BBT", "BDX", "BBBY", "BRK-B", "BBY", "BIIB", "BLK", "HRB", "BA", "BWA", "BXP", "BSX", "BMY", "BRCM", "BF-B", "CHRW", "CA", "CVC", "COG", "CAM", "CPB", "COF", "CAH", "HSIC", "KMX", "CCL", "CAT", "CBG", "CBS", "CELG", "CNP", "CTL", "CERN", "CF", "SCHW", "CHK", "CVX", "CMG", "CB", "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CTXS", "CLX", "CME", "CMS", "COH", "KO", "CCE", "CTSH", "CL", "CPGX", "CMCSA", "CMCSK", "CMA", "CSC", "CAG", "COP", "CNX", "ED", "STZ", "GLW", "COST", "CCI", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DLPH", "DAL", "XRAY", "DVN", "DO", "DFS", "DISCA", "DISCK", "DG", "DLTR", "D", "DOV", "DOW", "DPS", "DTE", "DD", "DUK", "DNB", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMC", "EMR", "ENDP", "ESV", "ETR", "EOG", "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ES", "EXC", "EXPE", "EXPD", "ESRX", "XOM", "FFIV", "FB", "FAST", "FDX", "FIS", "FITB", "FSLR", "FE", "FISV", "FLIR", "FLS", "FLR", "FMC", "FTI", "F", "FOSL", "BEN", "FCX", "FTR", "GME", "GPS", "GRMN", "GD", "GE", "GGP", "GIS", "GM", "GPC", "GNW", "GILD", "GS", "GT", "GWW", "HAL", "HBI", "HOG", "HAR", "HRS", "HIG", "HAS", "HCA", "HCP", "HP", "HES", "HPQ", "HD", "HON", "HRL", "HST", "HCBK", "HUM", "HBAN", "ITW", "IR", "INTC", "ICE", "IBM", "IP", "IPG", "IFF", "INTU", "ISRG", "IVZ", "IRM", "JEC", "JBHT", "JNJ", "JCI", "JPM", "JNPR", "KSU", "K", "KEY", "GMCR", "KMB", "KIM", "KMI", "KLAC", "KSS", "KHC", "KR", "LB", "LLL", "LH", "LRCX", "LM", "LEG", "LEN", "LVLT", "LUK", "LLY", "LNC", "LLTC", "LMT", "L", "LOW", "LYB", "MTB", "MAC", "M", "MNK", "MRO", "MPC", "MAR", "MMC", "MLM", "MAS", "MA", "MAT", "MKC", "MCD", "MHFI", "MCK", "MJN", "WRK", "MDT", "MRK", "MET", "KORS", "MCHP", "MU", "MSFT", "MHK", "TAP", "MDLZ", "MON", "MNST", "MCO", "MS", "MOS", "MSI", "MUR", "MYL", "NDAQ", "NOV", "NAVI", "NTAP", "NFLX", "NWL", "NFX", "NEM", "NWSA", "NWS", "NEE", "NLSN", "NKE", "NI", "NBL", "JWN", "NSC", "NTRS", "NOC", "NRG", "NUE", "NVDA", "ORLY", "OXY", "OMC", "OKE", "ORCL", "OI", "PCAR", "PH", "PDCO", "PAYX", "PYPL", "PNR", "PBCT", "POM", "PEP", "PKI", "PRGO", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PBI", "PCL", "PNC", "RL", "PPG", "PPL", "PX", "PCP", "PCLN", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PSA", "PHM", "PVH", "QRVO", "PWR", "QCOM", "DGX", "RRC", "RTN", "O", "RHT", "REGN", "RF", "RSG", "RAI", "RHI", "ROK", "COL", "ROP", "ROST", "RCL", "R", "CRM", "SNDK", "SCG", "SLB", "SNI", "STX", "SEE", "SRE", "SHW", "SIAL", "SIG", "SPG", "SWKS", "SLG", "SJM", "SNA", "SO", "LUV", "SWN", "SE", "STJ", "SWK", "SPLS", "SBUX", "HOT", "STT", "SRCL", "SYK", "STI", "SYMC", "SYY", "TROW", "TGT", "TEL", "TE", "TGNA", "THC", "TDC", "TSO", "TXN", "TXT", "HSY", "TRV", "TMO", "TIF", "TWX", "TWC", "TJX", "TMK", "TSS", "TSCO", "RIG", "TRIP", "FOXA", "FOX", "TSN", "TYC", "USB", "UA", "UNP", "UAL", "UNH", "UPS", "URI", "UTX", "UHS", "UNM", "URBN", "VFC", "VLO", "VAR", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "VIAB", "V", "VNO", "VMC", "WMT", "WBA", "DIS", "WM", "WAT", "ANTM", "WFC", "HCN", "WDC", "WU", "WY", "WHR", "WFM", "WMB", "WEC", "WYN", "WYNN", "XEL", "XRX", "XLNX", "XL", "XYL", "YHOO", "YUM", "ZBH", "ZION", "ZTS"];
 
@@ -18,21 +22,9 @@ mongoClient.connect(mongoUrl, function (error, database) {
     db = database;
     mongoError = error;
     console.log("starting....");
-    checkActivePredictions();
-    //grabAndInsertNewStockValues(["MMM", "ABT", "ABBV", "ACN", "ACE", "ATVI", "ADBE", "ADT", "AAP", "AES", "AET", "AFL", "AMG"]);
-    /*setTimeout(function() {
-        checkActivePredictions();
-    }, 5000);*/
-    //getHistoricalData(["AAPL", "GOOG"]);
+    //checkActivePredictions();
 
-    //checkActivePredictions(); // testing check predictions
-
-    /*var yesterdayDate = new Date();
-    yesterdayDate.setDate(yesterdayDate.getDate() - 1); // get yesterday's date
-    var yesterdayYear = yesterdayDate.getFullYear();
-    var yesterdayMonth = yesterdayDate.getMonth();
-    var yesterdayDay = yesterdayDate.getDate();
-    getHistoricalData(symbols, yesterdayDay, yesterdayMonth, yesterdayYear);*/
+    sendEmailToPredictor("adahan93@gmail.com", "false", "Your prediction was whack partnah.");
 });
 
 // configuring express to use body-parser as middle-ware.
@@ -221,9 +213,6 @@ var checkActivePredictions = function () {
                                 // grab this prediction's object's most recent value
                                 var mostRecentValueDocument = recentValues[symbolsIndicies[thisPrediction.object]];
 
-                                //USE for further DEBUGGING!!
-                                console.log("checking " + thisPrediction.object + " with action: " + thisPrediction.action + " " + thisPrediction.value.toString() + " at val: " + mostRecentValueDocument.value.toString());
-
                                 // the object that will be used to update the prediction
                                 // (assuming it's status has found to have changed from active)
                                 var predictionUpdateObject = {};
@@ -277,10 +266,16 @@ var checkActivePredictions = function () {
 
                                 // update the prediction if it's status was found to have changed from active
                                 if (predictionUpdateObject.$set.status != "active") {
+
+                                    // update prediction in the predictions collection
                                     db.collection('predictions').updateOne(
                                       { _id: thisPrediction._id },
                                       predictionUpdateObject,
                                       function (err, results) {});
+
+                                    // send email notifying the predictor of the change in status
+                                    sendEmailToPredictor(predictionUpdateObject.$set.status,
+                                        predictionUpdateObject.$set.reason);
                                 }
                             }
                         }
@@ -290,6 +285,52 @@ var checkActivePredictions = function () {
         });
     }
 }
+
+var sendEmailToPredictor = function(predictorEmail, predictionStatus, predictionReason) {
+    var smtpTransport = nodemailer.createTransport("SMTP",{
+        host: "mail.gandi.net", // hostname
+        secureConnection: true, // use SSL
+        port: 465, // port for secure SMTP
+        auth: {
+            user: "predict@toine.io",
+            pass: ""
+        }
+    });
+
+    var htmlEmail = "<div style='";
+    if(predictionStatus == "true") {
+        htmlEmail += "color: #53B981;";
+    }
+    else {
+        htmlEmail += "color: red;";
+    }
+    htmlEmail += "'>";
+    htmlEmail += "<h1>Your prediction turned out to be " + predictionStatus + ".</h1>";
+    htmlEmail += "<br/>";
+    htmlEmail += "<h3>" + predictionReason + "</h3>";
+    htmlEmail += "</div>";
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: "Predict.toine.io ✔ <predict@toine.io>", // sender address
+        to: predictorEmail, // list of receivers
+        subject: "The result of your prediction is in... ", // Subject line
+        text: "",
+        html: htmlEmail
+    };
+
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+
+        // if you don't want to use this transport object anymore, uncomment following line
+        smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+};
 
 var getReasonMessage = function(predictionDocument, mostRecentValueDocument, isPredictionSuccessful) {
     var mostRecentValueDate = new Date(0);
